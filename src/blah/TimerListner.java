a7b9c907eabd377e21c2addba3dd35f53eb7289a
bohnernj <bohnernj@rose-hulman.edu>
2019-11-13 17:23:11 -0500

    Fun...

---
+++ src/blah/TimerListner.java
@@ -0,0 +1,30 @@
+package blah;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * 
+ * Creates a timer for the game which allows the component to update
+ * as time goes on.
+ * 
+ *
+ */
+public class TimerListner implements ActionListener {
+
+	private GuiComponenet component;
+	
+	public TimerListner(GuiComponenet component) {
+		this.component = component;
+	}
+	
+	/**
+	 * While the game is not paused, updates the screen once every delay period
+	 * 
+	 * @param 
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+//		if (!this.component.pauseGame()) this.component.updateScreen();
+	}
+
+}
